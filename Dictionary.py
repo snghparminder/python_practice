a1224a236e46bb45a3ac10fcc7935816aa681a47
Parminder <snghparminder@yahoo.co.in>
2022-07-02 19:46:30 +0530

    this file don't have copy code from one dictionary to another

---
+++ Dictionary.py
@@ -0,0 +1,31 @@
+my_dic={"company":"Hundai",
+        "model":"i0","year":2022}
+# #Approach 1
+# print(my_dic["company"])
+# #approach 2
+# print(my_dic.get("year"))
+#change values of keys
+# my_dic["company"]="Maruti"
+# print(my_dic)
+#extract key value pair for dictionary
+# for x,y in (my_dic.items()):
+#         print(x,y)
+#only values
+
+# for i in my_dic.values():
+#         print(i)
+#for i in my_dic:
+        #print(i)
+
+#Delete particular item n my dictionary
+my_dic={"company":"Hundai",
+        "model":"i0","year":2022}
+# my_dic.pop("company")
+# print(my_dic)
+#to add item in dictionary
+# my_dic["colour"]="Red"
+# print(my_dic)
+
+
+
+
